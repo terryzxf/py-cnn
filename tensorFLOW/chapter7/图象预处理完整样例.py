babf4b8cfd9d4b6f07bad077adac22f9599641bf
terryzxf <terry_zxf@163.com>
2018-12-10 14:27:23 +0800

    '2018-12-10 '

--- tensorFLOW/chapter7/图象预处理完整样例.py
+++ tensorFLOW/chapter7/图象预处理完整样例.py
@@ -1,6 +1,7 @@
 import tensorflow as tf
 import matplotlib.pyplot as plt
 import numpy as np
+import sys
 
 
 def distort_color(image, color_ordering=0):
@@ -73,7 +74,7 @@ def preprocess_for_train(image, height, width, bbox):
 
     return distorted_image
 
-image_raw_data = tf.gfile.FastGFile("/path/picture/", "r").read()
+image_raw_data = tf.gfile.FastGFile(sys.path[0]+"/path/picture/1", "r").read()
 
 with tf.Session() as sess:
     img_data = tf.image.decode_jpeg(image_raw_data)
