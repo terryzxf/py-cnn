a64fdef89bd3463106da2c843b75ad36d4ac14b7
terryzxf <terry_zxf@163.com>
2018-12-10 14:05:19 +0800

    '2018-12-10 '

---
+++ tensorFLOW/chapter7/图象预处理完整样例.py
@@ -0,0 +1,87 @@
+import tensorflow as tf
+import matplotlib.pyplot as plt
+import numpy as np
+
+
+def distort_color(image, color_ordering=0):
+
+    if color_ordering == 0:
+        image = tf.image.random_brightness(image, max_delta=32./255.)
+        image = tf.image.random_saturation(image, lower=0.5, upper=1.5)
+        image = tf.image.random_hue(image,max_delta=0.2)
+        image = tf.image.random_contrast(image, lower=0.5, upper=1.5)
+
+    elif color_ordering == 1:
+        image = tf.image.random_saturation(image, lower=0.5, upper=1.5)
+        image = tf.image.random_contrast(image, lower=0.5, upper=1.5)
+        image = tf.image.random_brightness(image, max_delta=32. / 255.)
+        image = tf.image.random_hue(image, max_delta=0.2)
+
+    elif color_ordering == 2:
+
+        image = tf.image.random_hue(image, max_delta=0.2)
+        image = tf.image.random_contrast(image, lower=0.5, upper=1.5)
+        image = tf.image.random_brightness(image, max_delta=32. / 255.)
+        image = tf.image.random_saturation(image, lower=0.5, upper=1.5)
+
+    elif color_ordering == 3:
+
+        image = tf.image.random_contrast(image, lower=0.5, upper=1.5)
+        image = tf.image.random_saturation(image, lower=0.5, upper=1.5)
+        image = tf.image.random_hue(image, max_delta=0.2)
+        image = tf.image.random_brightness(image, max_delta=32. / 255.)
+
+    elif color_ordering == 4:
+
+        image = tf.image.random_contrast(image, lower=0.5, upper=1.5)
+        image = tf.image.random_brightness(image, max_delta=32. / 255.)
+
+        image = tf.image.random_hue(image, max_delta=0.2)
+        image = tf.image.random_saturation(image, lower=0.5, upper=1.5)
+
+    elif color_ordering == 5:
+
+        image = tf.image.random_contrast(image, lower=0.5, upper=1.5)
+        image = tf.image.random_brightness(image, max_delta=32. / 255.)
+        image = tf.image.random_hue(image, max_delta=0.2)
+        image = tf.image.random_saturation(image, lower=0.5, upper=1.5)
+
+
+    elif color_ordering == 6:
+
+        image = tf.image.random_contrast(image, lower=0.5, upper=1.5)
+        image = tf.image.random_hue(image, max_delta=0.2)
+        image = tf.image.random_brightness(image, max_delta=32. / 255.)
+        image = tf.image.random_saturation(image, lower=0.5, upper=1.5)
+
+def preprocess_for_train(image, height, width, bbox):
+
+    if bbox is None:
+        bbox = tf.constant([0.0, 0.0, 1.0, 1.0], dtype=tf/float32, shape=[1,1,4])
+
+    if image.dtype None= tf.float32:
+        iamge = tf.image.convert_image_dtype(image,dtype=tf.float32)
+    bbox_begin, bbox_size, _ = tf.image.sample_distorted_bounding_box(tf.shape(image), bounding_boxes=bbox)
+    distorted_image = tf.slice(image, bbox_begin, bbox_size)
+
+    distorted_image = tf.image.resize_images(
+        distorted_image, [height, width], method=np.random.randint(4)
+    )
+
+    distorted_image = tf.image.random_flip_left_right(distorted_image)
+    distorted_image = distort_color(distorted_image, np.random.randint(2))
+
+    return distorted_image
+
+image_raw_data = tf.gfile.FastGFile("/path/to/picture/", "r").read()
+
+with tf.Session() as sess:
+    img_data = tf.image.decode_jpeg(image_raw_data)
+    boxes = tf.constant([[0.05, 0.05, 0.9, 0.7], [0.35, 0.47, 0.5, 0.56]])
+
+    for i in range(6):
+
+        result = preprocess_for_train(img_data, 299, 299, boxes)
+
+        plt.imshow(result.eval())
+        plt.show()
