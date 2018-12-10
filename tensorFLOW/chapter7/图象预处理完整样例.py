5c405621d6b61d34afcf64bdd07c4880397b4a7c
terryzxf <terry_zxf@163.com>
2018-12-10 17:09:49 +0800

    '2018-12-10 '

--- tensorFLOW/chapter7/图象预处理完整样例.py
+++ tensorFLOW/chapter7/图象预处理完整样例.py
@@ -47,7 +47,6 @@ def distort_color(image, color_ordering=0):
         image = tf.image.random_hue(image, max_delta=0.2)
         image = tf.image.random_saturation(image, lower=0.5, upper=1.5)
 
-
     elif color_ordering == 6:
 
         image = tf.image.random_contrast(image, lower=0.5, upper=1.5)
@@ -55,14 +54,19 @@ def distort_color(image, color_ordering=0):
         image = tf.image.random_brightness(image, max_delta=32. / 255.)
         image = tf.image.random_saturation(image, lower=0.5, upper=1.5)
 
+    return tf.clip_by_value(image, 0.0, 1.0)
+
+
 def preprocess_for_train(image, height, width, bbox):
 
     if bbox is None:
-        bbox = tf.constant([0.0, 0.0, 1.0, 1.0], dtype=tf/float32, shape=[1,1,4])
+        bbox = tf.constant([0.0, 0.0, 1.0, 1.0], dtype=tf.float32, shape=[1, 1, 4])
 
     if image.dtype != tf.float32:
-        iamge = tf.image.convert_image_dtype(image,dtype=tf.float32)
-    bbox_begin, bbox_size, _ = tf.image.sample_distorted_bounding_box(tf.shape(image), bounding_boxes=bbox)
+        image = tf.image.convert_image_dtype(image, dtype=tf.float32)
+    # print(image.shape,'*****\n', bbox.shape)
+    bbox_begin, bbox_size, _ = tf.image.sample_distorted_bounding_box(tf.shape(image), bounding_boxes=bbox,
+                                                                      min_object_covered=0)
     distorted_image = tf.slice(image, bbox_begin, bbox_size)
 
     distorted_image = tf.image.resize_images(
@@ -74,11 +78,12 @@ def preprocess_for_train(image, height, width, bbox):
 
     return distorted_image
 
-image_raw_data = tf.gfile.FastGFile(sys.path[0]+"/path/123.jpg", "r").read()
+
+image_raw_data = tf.gfile.FastGFile(sys.path[0]+"/path/picture/123.png", "rb").read()
 
 with tf.Session() as sess:
     img_data = tf.image.decode_jpeg(image_raw_data)
-    boxes = tf.constant([[0.05, 0.05, 0.9, 0.7], [0.35, 0.47, 0.5, 0.56]])
+    boxes = tf.constant([[[0.05, 0.05, 0.9, 0.7], [0.35, 0.47, 0.5, 0.56]]])
 
     for i in range(6):
 
