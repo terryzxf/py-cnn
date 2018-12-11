d0c7663dd6123197d0573b6b5e3afee544424e3d
terryzxf <terry_zxf@163.com>
2018-12-11 16:51:57 +0800

    '2018-12-11'

--- tensorFLOW/chapter7/队列与多线程/输入文件队列.py
+++ tensorFLOW/chapter7/队列与多线程/输入文件队列.py
@@ -20,12 +20,15 @@
 #
 import tensorflow as tf
 
+
 # 创建TFRecord文件的帮助函数.
+
 def _int64_feature(value):
     return tf.train.Feature(int64_list=tf.train.Int64List(value=[value]))
 
 # 模拟海量数据情况下将数据写入不同的文件.num_shards定义了总共写入多少文件
 # instance_per_shard 定义了每个文件有多少个数据.
+
 
 num_shards = 2
 
@@ -33,17 +36,21 @@ instances_per_shard = 2
 
 for i in range(num_shards):
 
-    #
+    # 将数据分为多个文件时,可以将不同文件以类似0000n-of-0000m的后缀区分.
+    # 其中m表示了数据总共被存在了多少个文件中,n表示当前文件编号.
+    # 式样的方式既方便通过正则表达式获取文件列表中,又在文件名中加入了更多的信息.
     #
     #
     filename = ('./path/to/data.tfrecords-%.5d-of-%.5d' % (i, num_shards))
     writer= tf.python_io.TFRecordWriter(filename)
     # 将数据封装成Example结构并写入TFRecord文件.
     for j in range(instances_per_shard):
-        #
+        # Example结构仅包含当前样例属于第几个文件以及是当前文件的第几个样本.
         example = tf.train.Example(features=tf.train.Features(feature={
             'i': _int64_feature(i),
             'j': _int64_feature(j),
         }))
         writer.write(example.SerializeToString())
     writer.close()
+
+#
