6ac9d5a8b5f6c4b17d9b1732c20f53fb7316bfa8
zxf <terry_zxf@163.com>
2018-12-15 16:52:16 +0800

    2018 12 11 night  pycharm Setting

--- tensorFLOW/chapter7/队列与多线程/7.3.4输入数据处理框架.py
+++ tensorFLOW/chapter7/队列与多线程/7.3.4输入数据处理框架.py
@@ -1,6 +1,7 @@
 # 在前面的小节中已经介绍了图7-14中展示的流程图中的所有步骤。在这一小节将把这些步骤穿成一个完整的
 # tensorflow来处理输入数据。一下代码给出了这个完成的程序。
 import tensorflow as tf
+import numpy as np
 
 
 # 创建文件列表，通过文件列表创建文件输入队列。在调研输入数据处理流程，需要同意所以原始数据的格式并将它们存储到
@@ -102,11 +103,12 @@ height, width = features['height'],features['width']
 channels = features['channels']
 
 decoded_image = tf.decode_raw(image,tf.uint8)
+print(height,'\n',width,'\n',channels)
 decoded_image.set_shape([height,width,channels])
 
 image_size =299
 
-distortde_imge = preprocess_for_train(decoded_image, image_size, image_size, None)
+distorted_image = preprocess_for_train(decoded_image, image_size, image_size, None)
 
 
 min_after_dequeue = 10000
